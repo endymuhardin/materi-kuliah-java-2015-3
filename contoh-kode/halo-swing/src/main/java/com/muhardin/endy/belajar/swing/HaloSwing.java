0d89e3df147d513c4bbb3710cd234d89bba07665
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-25 09:47:45 +0700

    contoh aplikasi desktop sederhana

---
+++ contoh-kode/halo-swing/src/main/java/com/muhardin/endy/belajar/swing/HaloSwing.java
@@ -0,0 +1,16 @@
+package com.muhardin.endy.belajar.swing;
+
+import javax.swing.JFrame;
+
+public class HaloSwing {
+
+	public static void main(String[] x){
+		JFrame fr = new JFrame();
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.setTitle("Halo Swing");
+		fr.setSize(400,300);
+		fr.setLocationRelativeTo(null);
+		fr.setVisible(true);
+	}
+
+}
